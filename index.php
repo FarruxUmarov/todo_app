d9b20f718ee9d7c7ebab914d45522410f80f3acd
Farrukh <adler4313@gmail.com>
2024-07-18 12:48:31 +0500

    mysql is connected

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
-<form action="index.php">
-    <input type="text">
+<?php
+
+require 'DB.php';
+
+$pdo = DB::connect();
+$status = false;
+
+$stmt = $pdo->prepare("INSERT INTO todos(text, status) VALUES(:text, :status)");
+$stmt->bindParam(':text', $_POST['text']);
+$stmt->bindParam(':status', $status, PDO::PARAM_BOOL);
+$stmt->execute();
+// header("location: index.php");
+
+$todoList = $pdo->query("SELECT * FROM todos")->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<ul>
+    <?php 
+    foreach ($todoList as $item):
+    echo "<li>{$item['text']}</li>";
+    endforeach; ?>
+</ul>
+
+<form action="index.php" method="post">
+    <input type="checkbox" name="checkbox">
+    <input type="text" name="text">
+    <button type="Submit">Submit</button>
+
 </form>
 
 
